9941f580812a747551b05ec08591855a2e36c3da
victor <joaorodrigues2012.jr@gmail.com>
2019-03-27 18:31:13 -0300

    Aula 04

--- app/src/main/java/com/example/temperatura/LIstLocalizacaoActivity.java
+++ app/src/main/java/com/example/temperatura/LIstLocalizacaoActivity.java
@@ -2,15 +2,13 @@ package com.example.temperatura;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class LIstLocalizacaoActivity extends AppCompatActivity {
 
@@ -27,8 +25,9 @@ public class LIstLocalizacaoActivity extends AppCompatActivity {
         final ArrayList<Localizacao> localizacoes = (ArrayList<Localizacao>) origemIntent.getSerializableExtra("localizacoes");
         localizacaoListView = findViewById(R.id.localizacaoListView);
         //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chamados);
-        ArrayAdapter<Localizacao> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, localizacoes);
-        localizacaoListView.setAdapter(adapter);
+        //ArrayAdapter<Localizacao> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, localizacoes);
+        LocalizacaoArrayAdapter arrayAdapter = new LocalizacaoArrayAdapter(this,localizacoes);
+        localizacaoListView.setAdapter(arrayAdapter);
 
         localizacaoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
